9ec8f1e7aa78489776c62af3d7989862f8ca05bc
Alessandro Del Sole <alessandro.delsole@visual-basic.it>
2016-07-01 18:03:58 +0200

    Fixed NuGet metadata and XML comments

--- SQLiteConnection/Plugin.SQLiteConnection.Abstractions/ISQLiteConnection.cs
+++ SQLiteConnection/Plugin.SQLiteConnection.Abstractions/ISQLiteConnection.cs
@@ -8,8 +8,16 @@ namespace Plugin.SQLiteConnection.Abstractions
   /// </summary>
   public interface ISQLiteConnection
   {
+        /// <summary>
+        /// Create a connection to the specified SQLite database
+        /// </summary>
+        /// <param name="dbFileName"></param>
         void CreateConnection(string dbFileName);
 
+        /// <summary>
+        /// Return the connection to a SQLite database
+        /// Assumes you invoked CreateConnection first
+        /// </summary>
         SQLite.SQLiteConnection Connection { get; }
   }
 }
